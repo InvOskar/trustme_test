95c6492faf630130149c5a840f4e10ae2e03b92e
InvOskar <oskarinvr@gmail.com>
2025-06-19 23:40:32 +0500

    init

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="header">
+    <img class="header-logo" src="@/assets/logo_trustme.png" />
+    <div class="header-links">
+      <p>+7 777 777 77 77</p>
+      <p>Подать объявление</p>
+      <p>
+        <UserOutlined />
+        Войти/Регистрация
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+.header {
+  background: #f5f2f2;
+  padding: 0 10%;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+
+  &-logo {
+    height: 90%;
+  }
+  &-links {
+    display: flex;
+    gap: 24px;
+    & > p {
+      color: #5956e0;
+      &:hover {
+        cursor: pointer;
+        color: #0d0b97;
+      }
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+import Antd from 'ant-design-vue';
+import * as AntIcons from '@ant-design/icons-vue';
+
+Object.keys(AntIcons).forEach(key => {
+  app.component(key, AntIcons[key]);
+});
+app.use(Antd);
+
+app
+  .use(store)
+  .use(router)
+  .mount("#app");
